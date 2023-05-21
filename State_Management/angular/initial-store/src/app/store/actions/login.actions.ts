33cae29f804145825b2c6ebc41530f5f3c3412cc
zegis <kjgm@poczta.onet.pl>
2023-05-22 00:39:02 +0200

    Make action names more consistent with best naming practise

--- State_Management/angular/initial-store/src/app/store/actions/login.actions.ts
+++ State_Management/angular/initial-store/src/app/store/actions/login.actions.ts
@@ -1,4 +1,4 @@
 import { createAction } from "@ngrx/store";
 
-export const invalid_login = createAction("Login - invalid login");
-export const valid_login = createAction("Login - Login valid");
+export const invalid_login = createAction("[Login Page] Invalid login");
+export const valid_login = createAction("[Login Page] Login valid");
