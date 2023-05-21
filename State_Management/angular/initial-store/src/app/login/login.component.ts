3da9e30f43b3c763e7ee2dfc2b6430ea5076fc2a
zegis <kjgm@poczta.onet.pl>
2023-05-22 00:35:15 +0200

    Wired up login component with initial store

--- State_Management/angular/initial-store/src/app/login/login.component.ts
+++ State_Management/angular/initial-store/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { valid_login, invalid_login } from '../store/actions/login.actions';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  count$: Observable<number>;
 
+  constructor(private store: Store<{login: number}>) {
+    this.count$ = store.select('login')
+  }
+
+  valid() {
+    this.store.dispatch(valid_login());
+  }
+
+  invalid() {
+    this.store.dispatch(invalid_login());
+  }
 }
