392c8bda9217be5d21063279e1a0028bb57bc374
zegis <kjgm@poczta.onet.pl>
2023-05-22 10:31:41 +0200

    Added few more actions for more sophisticated store

--- State_Management/angular/initial-store/src/app/store/actions/login.actions.ts
+++ State_Management/angular/initial-store/src/app/store/actions/login.actions.ts
@@ -1,4 +1,8 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
+
 
 export const invalid_login = createAction("[Login Page] Invalid login");
 export const valid_login = createAction("[Login Page] Login valid");
+export const lock_account = createAction("[Login Page] - lock account");
+export const unlock_account = createAction("[Login Page] - account unlocked");
+export const set_login_data = createAction("[Login Page] - set login data", props<{login_count: number, locked: boolean}>() );
