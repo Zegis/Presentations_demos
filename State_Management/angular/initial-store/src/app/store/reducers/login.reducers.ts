e12ab004b6534e5ae395843d7e752bef7b037726
zegis <kjgm@poczta.onet.pl>
2023-05-22 10:55:47 +0200

    Created reducer functions for store with 2 properties

--- State_Management/angular/initial-store/src/app/store/reducers/login.reducers.ts
+++ State_Management/angular/initial-store/src/app/store/reducers/login.reducers.ts
@@ -1,10 +1,21 @@
 import { createReducer, on } from "@ngrx/store";
-import { invalid_login, valid_login } from "../actions/login.actions";
+import * as LoginActions from "../actions/login.actions";
 
-export const initial_state = 0;
+export interface LoginState {
+    invalid_login_tries: number,
+    account_locked: boolean
+}
+
+export const initial_state: LoginState = {
+    invalid_login_tries: 0,
+    account_locked: false
+}
 
 export const loginReducer = createReducer(
     initial_state,
-    on(invalid_login, (state) => state + 1),
-    on(valid_login, (state) => 0)
+    on(LoginActions.invalid_login, state => ({...state, invalid_login_tries: state.invalid_login_tries + 1})),
+    on(LoginActions.valid_login, state => ({invalid_login_tries: 0, account_locked: false})),
+    on(LoginActions.lock_account, state => ({...state, account_locked: true})),
+    on(LoginActions.unlock_account, state => ({...state, account_locked: false})),
+    on(LoginActions.set_login_data, (state, {login_count, locked}) => ({invalid_login_tries: login_count, account_locked: locked}))
     );
